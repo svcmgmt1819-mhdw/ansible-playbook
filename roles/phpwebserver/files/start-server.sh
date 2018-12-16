5a4d467ceba024dc711f7df8aa85259ca68cfa93
Marco Huenseler <marcoh.huenseler+git@gmail.com>
2018-12-16 19:16:28 +0100

    rename roles to make the intention clearer

--- roles/phpwebserver/files/start-server.sh
+++ roles/phpwebserver/files/start-server.sh
@@ -9,7 +9,7 @@ then
     echo "Delete pr08a2 in case it still exists"
     (docker rm pr08a2 || true) > /dev/null 2> /dev/null
     echo "Start container"
-    docker run -d -v /vagrant/roles/pr08/files/htmlpub:/var/www/html -p 0.0.0.0:20820:80 --name pr08a2 --rm php:7.2-apache > /dev/null 2> /dev/null
+    docker run -d -v /vagrant/roles/phpwebserver/files/htmlpub:/var/www/html -p 0.0.0.0:20820:80 --name pr08a2 --rm php:7.2-apache > /dev/null 2> /dev/null
     exit 0
 fi
 
